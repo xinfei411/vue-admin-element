b311e87e0cbd78342e93a059b2e3cf9247f6b837
yaowq <956625931@qq.com>
2018-12-26 17:53:12 +0800

    请求封装

--- src/page/Login.vue
+++ src/page/Login.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="login">
     <h1>{{ title }}</h1>
+    <button @click="login">登录</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "Login",
+  data() {
     return {
-      title: '欢迎登录'
+      title: "欢迎登录",
+      user:{
+        username:'ywq',
+        password:'123456',
+      }
+    };
+  },
+  methods: {
+    login(){
+      let vm=this;
+      vm.get('login.json',vm.user).then(data=>{
+        localStorage.token=data.token;
+        vm.$router.push('dashboard');
+      })
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
