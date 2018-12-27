5f7e8bc94ca2c0d9f1209c25dceb1c7b85442476
李玉霞 <01509084>
2018-12-27 17:39:41 +0800

    新增loading

--- src/page/Login.vue
+++ src/page/Login.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="login">
-    <h2>{{ title }}</h2>
-    <el-form :model="user" :rules="rules" ref="user" label-width="50px">
-      <el-form-item label="账号" prop="username">
-        <el-input v-model="user.username"></el-input>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input v-model="user.password" type="password"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="login('user')">登录</el-button>
-      </el-form-item>
-    </el-form>
+  <div class="login-box">
+    <div class="login">
+      <h2>{{ title }}</h2>
+      <el-form :model="user" :rules="rules" ref="user" label-width="50px">
+        <el-form-item label="账号" prop="username">
+          <el-input v-model="user.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="user.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="login('user')">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -51,11 +54,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.login-box{
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+}
 .login {
-  position: absolute;
+  /* position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%); */
   width: 400px;
   border: 1px solid #ccc;
   margin: auto;
