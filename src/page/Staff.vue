fd93de98e4818183156c2835c0618929ce28c788
李玉霞 <01509084>
2018-12-29 17:09:29 +0800

    获取员工列表 删除员工信息

--- src/page/Staff.vue
+++ src/page/Staff.vue
@@ -15,16 +15,16 @@
       class="center"
       height="calc(100% - 84px)"
     >
-      <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
+      <el-table-column prop="Role_name" label="昵称" width="300"></el-table-column>
+      <el-table-column fixed prop="departname" label="部门" width="150"></el-table-column>
       <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-      <el-table-column prop="province" label="省份" width="120"></el-table-column>
-      <el-table-column prop="city" label="市区" width="120"></el-table-column>
-      <el-table-column prop="address" label="地址" width="300"></el-table-column>
-      <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
+      <el-table-column prop="phone" label="手机号码" width="120"></el-table-column>
+      <el-table-column prop="tel" label="电话号码" width="120"></el-table-column>
+      <el-table-column prop="createdate" label="创建日期" width="120"></el-table-column>
       <el-table-column fixed="right" label="操作" width="150">
         <template slot-scope="scope">
           <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-          <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button>
+          <el-button @click="deleteOperation(scope.row)" type="text" size="small">删除</el-button>
           <el-button type="text" size="small">编辑</el-button>
         </template>
       </el-table-column>
@@ -43,6 +43,8 @@
 </template>
 
 <script>
+const getUserUrl = "https://www.wmstlink.com/lot/admin/get_user",
+deleteUrl = "https://www.wmstlink.com/lot/admin/del_user";
 export default {
   name: "Staff",
   data() {
@@ -51,42 +53,55 @@ export default {
       currentPage: 1,
       searchText:"",
       tableData: [
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        }
+        // {
+        //   id: "000001",
+        //   createdate: "20171223",
+        //   departid: "2",
+        //   departname: "登记部",
+        //   name: "陈明",
+        //   note: "",
+        //   phone: "18651647302",
+        //   tel: "111",
+        //   Role_id: "2f7aa2d4-4eda-45ec-9185-27f62c9b1474",
+        //   Role_name: "最牛操作员"
+        // }
+        
       ]
     };
   },
   methods: {
+    //获取用户信息
+    getUser(){
+      let vm = this;
+      ajax.post(getUserUrl,{}).then(resp=>{
+        vm.tableData = resp.user;
+      });
+    },
+    //删除操作
+    deleteOperation(row){
+      let vm = this;
+      vm.$confirm('确认删除员工信息?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          vm.deleteUser(row);//删除员工
+        }).catch(() => {
+                
+        });
+    },
+    //删除员工
+    deleteUser(row){
+      let vm = this;
+      //TODO:
+      //要支持删除多个员工
+      ajax.post(deleteUrl,{user_ids:[row.id]}).then(resp=>{
+        vm.$message({
+          message: '恭喜你，这是一条成功消息',
+          type: 'success'
+        });
+      });
+    },
     handleSizeChange(val) {
       console.log(`每页 ${val} 条`);
     },
