50a44adab8f20a6a5774623f541e3ba48255a25d
yaowq <956625931@qq.com>
2018-12-31 12:21:18 +0800

    刷新菜单状态保持

--- src/page/Layout.vue
+++ src/page/Layout.vue
@@ -12,14 +12,15 @@
     <div class="body">
       <el-menu
           unique-opened
+          :default-active="$route.path"
           class="el-menu-vertical-demo menu"
           >
-          <el-submenu :index="item.id" v-for="(item,index) in menu" :key="index">
+          <el-submenu :index="item.path" v-for="(item,index) in menu" :key="index">
             <template slot="title">
               <i :class="item.icon"></i>
               <span><router-link :to="item.path" tag="span">{{item.text}}</router-link></span>
             </template>
-            <el-menu-item :index="item.id" v-for="(item,index) in item.children" :key="index">
+            <el-menu-item :index="item.path" v-for="(item,index) in item.children" :key="index">
               <span slot="title"><router-link :to="item.path"  tag="span">{{item.text}}</router-link></span>
             </el-menu-item>
           </el-submenu>
